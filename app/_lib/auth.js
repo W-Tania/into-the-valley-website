be585e5e5df8a3d3431360c11f6de996f6dfc470
tania wang <tania.y.wang@gmail.com>
2024-06-28 15:48:09 +1000

    Connect Guest to Guest profile DataBase

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import { createGuest, getGuest } from "./data-service";
 
 const authConfig = {
   providers: [
@@ -13,6 +14,24 @@ const authConfig = {
       return !!auth?.user;
       //if there is a user, then return true
       //if not authenticated, NextAuth.js redirect to sign-in automatically based on callback configuration.
+    },
+    //check for related guest info in DB, if null, create new one
+    async signIn({ user, account, profile }) {
+      try {
+        const existingGuest = await getGuest(user.email);
+
+        if (!existingGuest)
+          await createGuest({ email: user.email, fullName: user.name });
+
+        return true;
+      } catch {
+        return false;
+      }
+    },
+    async session({ session, user }) {
+      const guest = await getGuest(session.user.email);
+      session.user.guestId = guest.id;
+      return session;
     },
   },
   pages: {
