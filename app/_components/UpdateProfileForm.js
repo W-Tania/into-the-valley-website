3e73ae7b285b08272d25ae2efd3722b5d45a3fd2
tania wang <tania.y.wang@gmail.com>
2024-06-28 20:10:35 +1000

    Update form loading indicator - useFormStatus

--- app/_components/UpdateProfileForm.js
+++ app/_components/UpdateProfileForm.js
@@ -1,6 +1,7 @@
 "use client";
 
 import { updateProfile } from "../_lib/actions";
+import { useFormStatus } from "react-dom";
 
 //to use state and hook, this form needs to be a client component.
 //but the <SelectCountry /> should be a server component, as fetching data needs environmental variable that should not reveal to client.
@@ -56,11 +57,25 @@ function UpdateProfileForm({ guest, children }) {
       </div>
 
       <div className="flex justify-end items-center gap-6">
-        <button className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
-          Update profile
-        </button>
+        <Button />
       </div>
     </form>
+  );
+}
+
+//want to display form status on button
+//the hook can not be used in the component which contains the form.
+//it needs to be inside a component that is rendered inside the form
+//* since using hook, the button needs to be client component
+function Button() {
+  const { pending } = useFormStatus();
+  return (
+    <button
+      className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300"
+      disabled={pending}
+    >
+      {pending ? "Updating..." : "Update profile"}
+    </button>
   );
 }
 
